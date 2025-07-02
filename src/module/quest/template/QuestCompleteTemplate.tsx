93c7dd14416eaf4a264c80ecdfecbb41fca60fa0
eungyu123 <150225773+eungyu123@users.noreply.github.com>
2025-07-02 11:16:34 +0900

    feat: 로띠

--- src/module/quest/template/QuestCompleteTemplate.tsx
+++ src/module/quest/template/QuestCompleteTemplate.tsx
@@ -1,13 +1,19 @@
 import { Background } from "@/components/layout/Background";
 import { TextWithStroke } from "@/components/text/TextWithStroke";
 import { IMAGE_URLS } from "@/lib/constants/constants";
+import { useState } from "react";
+import Lottie from "react-lottie";
+import animationData from '@/components/lottie/Confetti.json';
 
 interface QuestCompleteTemplate {
   onComplete: () => void;
 }
+
 export const QuestCompleteTemplate = ({
   onComplete,
 }: QuestCompleteTemplate) => {
+  const [shouldPlayAnimation, setShouldPlayAnimation] = useState(true);
+
   return (
     <Background backgroundImage={IMAGE_URLS.quest.quest_complete_bg}>
       <TextWithStroke
@@ -17,6 +23,36 @@ export const QuestCompleteTemplate = ({
                 text-[3rem]"
         strokeClassName=" text-main-brown-800 text-[3rem] text-stroke-width-[0.5rem]  text-stroke-color-main-brown-800"
       />
+      
+      {shouldPlayAnimation && (
+        <Lottie
+          options={{
+            loop: false,
+            autoplay: true,
+            animationData,
+            rendererSettings: {
+              preserveAspectRatio: "xMidYMid slice",
+            },
+          }}
+          height={"30rem"}
+          width={"32rem"}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            pointerEvents: "none",
+            zIndex: 10,
+          }}
+          eventListeners={[
+            {
+              eventName: "complete",
+              callback: () => setShouldPlayAnimation(false),
+            },
+          ]}
+        />
+      )}
+      
       <div
         onClick={onComplete}
         className="absolute bottom-[1rem] left-[15.5rem] text-main-brown-850 bg-[#ffdc3efe] text-brown-800 font-semibold text-[1rem] px-[1rem] py-[0.3rem] rounded-full shadow-lg border-2 border-main-brown-500 cursor-pointer"
